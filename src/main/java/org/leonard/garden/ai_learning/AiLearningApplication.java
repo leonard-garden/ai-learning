0af5c51bfac3f2e6970e04b429370547eb90f2ef
Leonard Trinh <trinhvandat90399@gmail.com>
2026-06-06 14:39:02 +0700

    initial commit

---
+++ src/main/java/org/leonard/garden/ai_learning/AiLearningApplication.java
@@ -0,0 +1,13 @@
+package org.leonard.garden.ai_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLearningApplication.class, args);
+	}
+
+}
